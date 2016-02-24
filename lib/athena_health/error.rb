c65ab4cf9d5c6d0941bdb13b1a2b553bf6884a01
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-02-24 11:06:33 +0100

    Refactor error class

--- lib/athena_health/error.rb
+++ lib/athena_health/error.rb
@@ -1,28 +1,4 @@
 module AthenaHealth
-  class Error
-    def initialize(code:)
-      @code = code
-    end
-
-    def render
-      fail errors[@code]
-    end
-
-    private
-
-    def errors
-      {
-        401 => UnauthorizedError,
-        402 => IncorrectPermissionsError,
-        403 => ForbiddenError,
-        404 => NotFoundError,
-        409 => ConflictError,
-        500 => InternalServerError,
-        503 => ServiceUnavailableError,
-      }
-    end
-  end
-
   class UnauthorizedError         < StandardError; end
   class IncorrectPermissionsError < StandardError; end
   class ForbiddenError            < StandardError; end
@@ -30,4 +6,24 @@ module AthenaHealth
   class ConflictError             < StandardError; end
   class InternalServerError       < StandardError; end
   class ServiceUnavailableError   < StandardError; end
+
+  class Error
+    ERROR_TYPES = {
+      401 => UnauthorizedError,
+      402 => IncorrectPermissionsError,
+      403 => ForbiddenError,
+      404 => NotFoundError,
+      409 => ConflictError,
+      500 => InternalServerError,
+      503 => ServiceUnavailableError,
+    }
+
+    def initialize(code:)
+      @code = code
+    end
+
+    def render
+      fail ERROR_TYPES[@code]
+    end
+  end
 end
