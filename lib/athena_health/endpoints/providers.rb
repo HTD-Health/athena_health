f455f752cc8be9dc004b81c538e38d718e549f5b
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-02-25 15:31:44 +0100

    Add fin_provider method to the client

--- lib/athena_health/endpoints/providers.rb
+++ lib/athena_health/endpoints/providers.rb
@@ -10,6 +10,16 @@ module AthenaHealth
 
         ProviderCollection.new(response)
       end
+
+      def find_provider(practice_id:, provider_id:, params: {})
+        response = @api.call(
+          endpoint: "#{practice_id}/providers/#{provider_id}",
+          method: :get,
+          params: params
+        )
+
+        Provider.new(response.first)
+      end
     end
   end
 end
--- spec/lib/endpoints/providers_spec.rb
+++ spec/lib/endpoints/providers_spec.rb
@@ -11,4 +11,20 @@ describe AthenaHealth::Endpoints::Providers do
       end
     end
   end
+
+  describe '#find_provider' do
+    let(:attributes) do
+      {
+        practice_id: 195_900,
+        provider_id: 86
+      }
+    end
+
+    it 'returns instance of Provider' do
+      VCR.use_cassette('find_provider') do
+        expect(client.find_provider(attributes))
+          .to be_an_instance_of AthenaHealth::Provider
+      end
+    end
+  end
 end
