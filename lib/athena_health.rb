74e0b310d6d10f714dff71fe2fba2f8e61f8b54f
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2016-06-03 11:14:41 +0200

    Add Insurance class

--- lib/athena_health.rb
+++ lib/athena_health.rb
@@ -59,6 +59,7 @@ require 'athena_health/note'
 require 'athena_health/note_collection'
 require 'athena_health/template'
 require 'athena_health/social_history'
+require 'athena_health/insurance'
 
 module AthenaHealth
 end
