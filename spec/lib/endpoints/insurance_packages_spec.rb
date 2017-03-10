46378e4b56e3ec815bd47a818f41c2815dae5fee
Lance Jordan <djlancejordan@gmail.com>
2017-03-10 17:23:09 -0500

    Fix Spelling Typo in Spec

--- spec/lib/endpoints/insurance_packages_spec.rb
+++ spec/lib/endpoints/insurance_packages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe AthenaHealth::Endpoints::InsurancePackages do
   let(:attributes) { { practice_id: 195900 } }
   describe '#top_insurance_packages' do
-    it 'returns instance of InsutancePackageCollection' do
+    it 'returns instance of InsurancePackageCollection' do
       VCR.use_cassette('top_insurance_packages') do
         expect(client.top_insurance_packages(attributes))
           .to be_an_instance_of AthenaHealth::InsurancePackageCollection
@@ -12,7 +12,7 @@ describe AthenaHealth::Endpoints::InsurancePackages do
   end
 
     describe '#common_insurance_packages' do
-      it 'returns instance of InsutancePackageCollection' do
+      it 'returns instance of InsurancePackageCollection' do
         VCR.use_cassette('common_insurance_packages') do
           expect(client.common_insurance_packages(practice_id: 195900, params: {departmentid: 1}))
             .to be_an_instance_of AthenaHealth::InsurancePackageCollection
