66b5a91a2428b6cb406ae77ec19db0d6d43fdfc1
Ben Jones <ben@benjones.io>
2021-07-22 11:27:09 -0400

    update specs to new version

--- spec/lib/client_spec.rb
+++ spec/lib/client_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe AthenaHealth::Client do
   describe '#initialize' do
     it 'creates new instance of AthenaHealth::Connection' do
-      expect(AthenaHealth::Connection).to receive(:new).with(client_attributes)
+      expect(AthenaHealth::Connection).to receive(:new).with(client_attributes.merge({production: false}))
 
       client
     end
