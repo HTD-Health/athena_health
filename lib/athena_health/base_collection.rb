44382481b7b43f2cd5751e7efb33155ae7447761
Ben Jones <ben@benjones.io>
2023-02-02 08:16:33 -0500

    unnecessary include

--- lib/athena_health/base_collection.rb
+++ lib/athena_health/base_collection.rb
@@ -1,8 +1,6 @@
 require_relative 'base_model'
 module AthenaHealth
   class BaseCollection < BaseModel
-    include Virtus.model
-
     attribute :totalcount, Integer
     attribute :next,       String
     attribute :previous,   String
